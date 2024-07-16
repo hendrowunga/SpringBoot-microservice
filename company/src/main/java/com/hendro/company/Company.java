b0839c1294d38ff2bff28d457eca43e005120010
Hendro <hendrowunga073@gmail.com>
2024-07-16 20:40:56 +0700

    Add code explanation

--- company/src/main/java/com/hendro/company/Company.java
+++ company/src/main/java/com/hendro/company/Company.java
@@ -1,24 +1,21 @@
 package com.hendro.company;
+import jakarta.persistence.Entity; // Mengimpor anotasi Entity dari package jakarta.persistence
+import jakarta.persistence.GeneratedValue; // Mengimpor anotasi GeneratedValue dari package jakarta.persistence
+import jakarta.persistence.Id; // Mengimpor anotasi Id dari package jakarta.persistence
+import lombok.*; // Mengimpor semua anotasi lombok, termasuk Getter, Setter, AllArgsConstructor, NoArgsConstructor, dan Builder
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-
+@Entity // Menandakan bahwa kelas ini adalah entitas yang dapat disimpan dalam database
+@Getter // Menghasilkan metode getter untuk semua bidang kelas
+@Setter // Menghasilkan metode setter untuk semua bidang kelas
+@AllArgsConstructor // Menghasilkan konstruktor dengan semua parameter
+@NoArgsConstructor // Menghasilkan konstruktor tanpa parameter
+@Builder // Menghasilkan metode builder untuk membuat objek instans dari kelas ini
 public class Company {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
-    private String name;
-    private String email;
+    @Id // Menandakan bahwa properti ini adalah primary key dalam tabel database
+    @GeneratedValue // Menandakan bahwa nilai properti ini akan secara otomatis di-generate oleh sistem
+    private Integer id; // Properti untuk menyimpan ID perusahaan
 
-
+    private String name; // Properti untuk menyimpan nama perusahaan
+    private String email; // Properti untuk menyimpan email perusahaan
 }
--- config-server/src/main/java/com/hendro/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/hendro/configserver/ConfigServerApplication.java
@@ -13,3 +13,13 @@ public class ConfigServerApplication {
 	}
 
 }
+
+/*
+  - @EnableConfigServer : Ini menandakan bahwa aplikasi ini adalah config Server.
+ 						  Config server digunkan untuk menyimpan dan mengelola konfigurasi terpusat untuk aplikasi lain.
+
+  - @SpringBootAplication : Anotasi ini menandakan bahwa ini adalah kelas utama dari apliasi Spring boot.
+  						 Ini menggabunkan beberapa anotasi lain seperti "@Configuration","@EnableAutoConfiguration",dan "@ComponentScan"
+  - public static void main(String[] args): Method ini adalah titik masuk utama aplikasi Java.
+  									Ketika aplikasi dijalankan, method ini akan memulai Spring application context dan menjalankan aplikasi Spring Boot.
+ */
